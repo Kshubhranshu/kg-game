9a2f003d66272cc8ed6eebe511861f21be43b08a
kshubhranshu <shubhranshu14@gmail.com>
2020-05-01 17:26:50 +0530

    Updates

---
+++ precache-manifest.5c75663e5bee0e66f41759e490809bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4304e6351fc7fb42998f49f65e2a47f7",
+    "url": "/kg-game/index.html"
+  },
+  {
+    "revision": "214a5d5485f4dc753cb4",
+    "url": "/kg-game/static/css/main.2fe966d0.chunk.css"
+  },
+  {
+    "revision": "b6fb17fa6da843d20ed6",
+    "url": "/kg-game/static/js/2.434287eb.chunk.js"
+  },
+  {
+    "revision": "86414f6aa96d78f04460ff2dda206a29",
+    "url": "/kg-game/static/js/2.434287eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "214a5d5485f4dc753cb4",
+    "url": "/kg-game/static/js/main.fc1ff0d0.chunk.js"
+  },
+  {
+    "revision": "3c56f571dd0b74d3db64",
+    "url": "/kg-game/static/js/runtime-main.b1caa8f3.js"
+  },
+  {
+    "revision": "97512cce2be46b8fa7db12fe44b21e85",
+    "url": "/kg-game/static/media/score.97512cce.svg"
+  }
+]);
